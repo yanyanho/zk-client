aafae992c0eff893df3c6852b6d11a0a38a501cc
youwenbusi <1245549353@qq.com>
2020-07-24 18:33:26 +0800

    transfer to fisco-bcos

---
+++ fisco_web3/eth_abi/__init__.py
@@ -0,0 +1,12 @@
+import pkg_resources
+
+from python_web3.eth_abi.abi import (  # NOQA
+    decode_abi,
+    decode_single,
+    encode_abi,
+    encode_single,
+    is_encodable,
+    is_encodable_type,
+)
+
+__version__ = "1.0"
