307ea0216001a0f0c52a7e9f007a411db53b916b
yanyanho <yanyanho@126.com>
2020-08-21 21:05:26 +0800

    delete the useless code

--- create_tables.sql
+++ create_tables.sql
@@ -1,12 +1,12 @@
-CREATE TABLE merkletree (
+CREATE TABLE if not exists merkletree (
   mid int NOT NULL,
   tree_data      text(40000) NOT NULL,
   blockNumber      int NOT NULL,
   last_modified_date  TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  created_date        TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_date        TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP
 )ENGINE = InnoDB;
 
-CREATE TABLE contract (
+CREATE TABLE if not exists contract (
   conName char(20) NOT NULL,
   conType  char(20) NOT NULL,
   conAddr text(500) NOT NULL,
@@ -14,10 +14,10 @@ CREATE TABLE contract (
   totalAmount bigint NOT NULL,
   shortName char(20) NOT NULL,
   last_modified_date  TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  created_date        TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_date        TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP
 )ENGINE = InnoDB;
 
-CREATE TABLE transactions (
+CREATE TABLE  if not exists transactions (
   traType char(20) NOT NULL,
   username  char(20) NOT NULL,
   vin int NOT NULL,
@@ -25,5 +25,5 @@ CREATE TABLE transactions (
   input_notes char(40),
   output_specs text(2000),
   last_modified_date  TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  created_date        TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_date        TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP
 )ENGINE = InnoDB;
